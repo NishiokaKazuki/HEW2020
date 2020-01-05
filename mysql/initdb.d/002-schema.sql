613272e6f7422cdcf4b88e4574f64329ddbeb677
NishiokaKazuki <n.kazuki.hal@gmail.com>
2020-01-05 15:00:39 +0900

    開発環境構築完了

---
+++ mysql/initdb.d/002-schema.sql
@@ -0,0 +1,118 @@
+SET CHARSET utf8;
+
+DROP TABLE IF EXISTS app_users;
+
+CREATE TABLE app_users
+(
+    id               bigint unsigned AUTO_INCREMENT,
+    name             text NOT NULL,
+    sex              int unsigned,
+    age              int unsigned,
+    sign_id          VARCHAR(255) unique NOT NULL,
+    sign_pw          VARCHAR(255) NOT NULL,
+    disabled         boolean DEFAULT NULL,
+    created_at       timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at       timestamp NOT NULL DEFAULT current_timestamp,
+    PRIMARY KEY (id)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+DROP TABLE IF EXISTS companies;
+
+CREATE TABLE companies
+(
+    id               bigint unsigned AUTO_INCREMENT,
+    name             text,
+    disabled         boolean DEFAULT NULL,
+    created_at       Datetime DEFAULT NULL,
+    updated_at       Datetime DEFAULT NULL,
+    PRIMARY KEY (id)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS stores;
+
+CREATE TABLE stores
+(
+    id               bigint unsigned AUTO_INCREMENT,
+    company_id       bigint unsigned,
+    image            text,
+    address          text,
+    disabled         boolean DEFAULT NULL,
+    created_at       Datetime DEFAULT NULL,
+    updated_at       Datetime DEFAULT NULL,
+    FOREIGN KEY (company_id) 
+    REFERENCES companies(id),
+    PRIMARY KEY (id)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE products
+(
+    id               bigint unsigned AUTO_INCREMENT,
+    name             text NOT NULL,
+    image            text,
+    price            int unsigned NOT NULL,
+    type             int unsigned NOT NULL,
+    disabled         boolean DEFAULT NULL,
+    created_at       Datetime DEFAULT NULL,
+    updated_at       Datetime DEFAULT NULL,
+   PRIMARY KEY (id)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS rfid_tags;
+
+CREATE TABLE rfid_tags
+(
+    id               bigint unsigned AUTO_INCREMENT,
+    product_id       bigint unsigned NOT NULL,
+    rfid_code        text NOT NULL,
+    sold             boolean DEFAULT NULL,
+    created_at       Datetime DEFAULT NULL,
+    updated_at       Datetime DEFAULT NULL,
+    FOREIGN KEY (product_id) 
+     REFERENCES products(id),
+    PRIMARY KEY (id)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS product_stocks;
+
+CREATE TABLE product_stocks
+(
+    store_id         bigint unsigned NOT NULL,
+    product_id       bigint unsigned NOT NULL,
+    stock            int    unsigned,
+    created_at       Datetime DEFAULT NULL,
+    updated_at       Datetime DEFAULT NULL,
+    FOREIGN KEY (store_id) 
+     REFERENCES stores(id),
+    FOREIGN KEY (product_id) 
+     REFERENCES products(id)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS bought_products;
+
+CREATE TABLE bought_products
+(
+    user_id          bigint unsigned NOT NULL,
+    product_id       bigint unsigned NOT NULL,
+    date             Datetime DEFAULT NULL,
+    created_at       Datetime DEFAULT NULL,
+    updated_at       Datetime DEFAULT NULL,
+    FOREIGN KEY (user_id) 
+     REFERENCES app_users(id),
+    FOREIGN KEY (product_id) 
+     REFERENCES products(id)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS face_ids;
+
+CREATE TABLE face_ids
+(
+    user_id          bigint unsigned NOT NULL,
+    image            text NOT NULL,
+    created_at       Datetime DEFAULT NULL,
+    updated_at       Datetime DEFAULT NULL,
+    FOREIGN KEY (user_id) 
+     REFERENCES app_users(id)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
