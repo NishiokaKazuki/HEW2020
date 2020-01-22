c3e4ce700783841f80a21c9c457694aed28cc110
hirokikondo86 <kondo.h.0806@gmail.com>
2020-01-23 02:44:10 +0900

    マップの表示

--- client/src/containers/Search/index.tsx
+++ client/src/containers/Search/index.tsx
@@ -1,17 +1,58 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import requestApi from '../../utils/requestApi'
 import styled from 'styled-components'
 
 import { makeStyles } from '@material-ui/core/styles'
 
+import GoogleMapReact from 'google-map-react'
+import { Me, Pin } from '../../components/MapIcons'
+
+interface iCenter {
+  lat: number,
+  lng: number
+}
+
 const Search: React.FC = () => {
   const classes = useStyles()
+  const [shop, setShop] = React.useState()
+  const [lat, setLat] = React.useState(0)
+  const [lng, setLng] = React.useState(0)
+  const center: iCenter = { lat: lat, lng: lng }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(pos => {
+      const lat = pos.coords.latitude
+      const lng = pos.coords.longitude
+      const res = requestApi(lat, lng)
+
+      setShop(res.results)
+      setLat(lat)
+      setLng(lng)
+    })
+  })
 
   return (
     <>
+      <div id="map"></div>
       <div className={classes.toolbar} />
-      <h1>Search</h1>
-      <h1>Search</h1>
-      <h1>Search</h1>
+      <H1>店舗検索</H1>
+
+      <GoogleMapWrapper>
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: 'AIzaSyDXmfufz94HbjCTfGwUBF4TCIwJ2_j-KW8'
+          }}
+          center={center}
+          defaultZoom={15}
+          yesIWantToUseGoogleMapApiInternals
+        >
+          <Me
+            lat={lat}
+            lng={lng}
+          />
+        </GoogleMapReact>
+      </GoogleMapWrapper>
     </>
   )
 }
@@ -19,5 +60,13 @@ const Search: React.FC = () => {
 const useStyles = makeStyles(theme => ({
   toolbar: theme.mixins.toolbar,
 }))
+
+const H1 = styled.h1`
+  text-align: center;
+`
+
+const GoogleMapWrapper = styled.div`
+  margin: 0 auto;
+`
 
 export default Search
