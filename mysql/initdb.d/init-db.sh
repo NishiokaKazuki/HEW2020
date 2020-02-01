1bce29a9e145badd807a17cbb315d5133f85416f
hirokikondo86 <kondo.h.0806@gmail.com>
2020-02-01 18:27:47 +0900

    cache clear

--- mysql/initdb.d/init-db.sh
+++
@@ -1,4 +0,0 @@
-# CreateTable
-mysql -u root -psecret < "/docker-entrypoint-initdb.d/001-init.sql"
-mysql -u root -psecret hew2020 < "/docker-entrypoint-initdb.d/002-schema.sql"
-mysql -u root -psecret hew2020 < "/docker-entrypoint-initdb.d/999-dummy.sql"
--- mysql/initdb.d/reset_dummy.sh
+++
@@ -1,1 +0,0 @@
-mysql -u root -psecret hew2020 < "/docker-entrypoint-initdb.d/999-dummy.sql"
