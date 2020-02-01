7a0384b730e99d0cad371afc56f45b6b3711ccdb
hirokikondo86 <kondo.h.0806@gmail.com>
2020-02-01 18:34:27 +0900

    [delete]delete proto

--- client/src/proto/mechanical_service_pb.d.ts
+++
@@ -1,6 +0,0 @@
-// package: services
-// file: mechanical_service.proto
-
-import * as jspb from "google-protobuf";
-import * as messages_pb from "./messages_pb";
-
--- client/src/proto/messages_pb_service.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable */
-// package: messages
-// file: messages.proto
-
